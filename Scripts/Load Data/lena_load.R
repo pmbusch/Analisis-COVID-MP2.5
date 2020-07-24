5c4a6368cd1cd0bc9ef63957228adeea86c130f5
Pablo <pmbusch@uc.cl>
2020-07-24 15:25:00 -0400

    Add data leña

---
+++ Scripts/Load Data/lena_load.R
@@ -0,0 +1,48 @@
+### Analisis-COVID-MP2.5
+## Carga de datos de consumo de leña
+## PBH Julio 2020
+
+# Fuente datos:
+# CDT 2015. MEDICIÓN DEL CONSUMO NACIONAL DE LEÑA Y OTROS COMBUSTIBLES SÓLIDOS DERIVADOS DE LA MADERA”. Tabla 136
+
+# source("Scripts/Load Data/mapa_load.R", encoding = "UTF-8")
+
+df_lena <- read_excel("Data/Data Modelo/ConsumoLena_CDT.xlsx", sheet="Consumo")
+
+## Bajamos los datos regiones de consumo de leña al 2018 a nivel comunal mediante la poblacion
+## Penetracion de lena es igual a nivel de region (no se tiene mayor detalle)
+df_poblacion$codigo_region %>% unique()
+
+df_poblacionZona <- df_poblacion %>% 
+  mutate(zona=case_when(
+    codigo_region %in% c("15","01","02","03","04") ~ "ZN",
+    codigo_region == "05" ~ "V",
+    codigo_region == "13" ~ "RM",
+    codigo_region == "06" ~ "VI",
+    codigo_region == "07" ~ "VII",
+    codigo_region %in% c("08","16") ~ "VIII",
+    codigo_region == "09" ~ "IX",
+    codigo_region == "14" ~ "XIV",
+    codigo_region == "10" ~ "X",
+    codigo_region == "11" ~ "XI",
+    codigo_region == "12" ~ "XII"))
+df_poblacionZona$zona %>% unique()
+
+df_poblacionZona <- df_poblacionZona %>% group_by(zona, codigo_comuna) %>%
+  summarise(pob=sum(poblacion,na.rm=T)) %>%
+  mutate(porc_pob=pob/sum(pob)) %>%
+  ungroup()
+
+
+df_lena <- left_join(df_poblacionZona, df_lena, by=c("zona")) %>%
+  mutate(consumo_lena_m3=consumo_lena_m3*porc_pob) %>%
+  rename(penetracion_lena=porcentaje_viviendas_lena) %>% 
+  select(codigo_comuna, consumo_lena_m3, penetracion_lena)
+
+## Check: Total debe ser 11,962,058
+df_lena$consumo_lena_m3 %>% sum()
+
+# Limpio WS
+rm(df_poblacionZona)
+
+## EoF
