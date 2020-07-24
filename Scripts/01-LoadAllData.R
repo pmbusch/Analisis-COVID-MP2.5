851f6f859f2010176f96b5b4ad3549b5705a0431
Pablo <pmbusch@uc.cl>
2020-07-24 17:02:42 -0400

    Update load data

--- Scripts/01-LoadAllData.R
+++ Scripts/01-LoadAllData.R
@@ -9,6 +9,8 @@ source("Scripts/00-CargaLibrerias.R", encoding = "UTF-8")
 source("Scripts/Load Data/mapa_load.R", encoding = "UTF-8")
 source("Scripts/Load Data/censo_load.R", encoding = "UTF-8")
 source("Scripts/Load Data/casen_load.R", encoding = "UTF-8")
+source("Scripts/Load Data/lena_load.R", encoding = "UTF-8") #debe ejecutarse dp de mapa_load
+
 
 ## Data ya cargada
 df_conc <- read_delim("Data/Data Modelo/Datos_Concentraciones.csv", 
@@ -17,9 +19,15 @@ df_conc <- read_delim("Data/Data Modelo/Datos_Concentraciones.csv",
                  locale = locale(encoding = "windows-1252"))
 
 
+df_meteo <- read_delim("Data/Data Modelo/Datos_Meteorologia.csv", 
+                      delim = ";", skip = 1, na = c("NA"),
+                      col_types = "dcdcccDc",
+                      locale = locale(encoding = "windows-1252"))
+
+
 ## Cargar nuevamente ------
 # Solamente ejecutar para actualizar datos
 source("Scripts/Load Data/sinca_scrap.R", encoding = "UTF-8") # SINCA
-
+source("Scripts/Load Data/meteo_scrap.R", encoding = "UTF-8") # MeteoChile
 
 ## EoF
