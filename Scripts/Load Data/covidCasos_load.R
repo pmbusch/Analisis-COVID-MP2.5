6b4d01ee0afc03b05737ea816795488b92a4f11a
Pablo <pmbusch@uc.cl>
2020-07-26 12:52:33 -0400

    Add data casos covid

---
+++ Scripts/Load Data/covidCasos_load.R
@@ -0,0 +1,42 @@
+### Analisis-COVID-MP2.5
+## Datos Covid. Fuente: https://github.com/MinCiencia/Datos-COVID19
+## Producto 1: Casos totales por comuna incremental
+## PBH Julio 2020
+
+
+## Descarga de datos ---------
+# Url para descarga directa de datos desde el Github del Ministerio de Ciencia
+url <- "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output"
+
+# Nota: Archivos _std vienen aplanados
+df_casos <- read_csv(paste(url,"producto1","Covid-19_std.csv", sep="/"))
+names(df_casos) <- names(df_casos) %>% str_to_lower() %>% str_replace_all(" ","_")
+df_casos <- df_casos %>% na.omit() # limpio NA
+
+## Dia primer contagio por comuna ---------------
+# Limitado por fecha de primer reporte 30-03-2020
+df_contagioZero <- df_casos %>% 
+  filter(casos_confirmados!=0) %>% #Filtro que haya casos
+  group_by(codigo_comuna,comuna) %>% 
+  summarise(dia_contagioZero=min(fecha, na.rm=T)) %>% ungroup()
+
+
+## Casos totales a la fecha -----------
+# Filtro fechas mas reciente
+fecha_casos <- df_casos$fecha %>% max()
+df_casos <- df_casos %>% filter(fecha==fecha_casos)
+
+# total casos confirmados
+df_casos$casos_confirmados %>% sum()
+df_casos$poblacion %>% sum()
+
+## Parametros --------
+df_casos <- df_casos %>% 
+  mutate(tasa_contagios=casos_confirmados/poblacion*1e5)
+  
+
+df_casos <- df_casos %>% left_join(df_contagioZero %>% select(-comuna))
+
+
+rm(url, df_contagioZero)
+## EoF
