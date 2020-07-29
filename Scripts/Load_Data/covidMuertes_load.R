552b6219c51e2155357fa64a725238c87832b2ea
Pablo <pmbusch@uc.cl>
2020-07-29 14:12:33 -0400

    Add dias desde primera muerte

--- Scripts/Load_Data/covidMuertes_load.R
+++ Scripts/Load_Data/covidMuertes_load.R
@@ -14,6 +14,14 @@ names(df_muertes) <- names(df_muertes) %>% str_to_lower() %>% str_replace_all(" 
 df_muertes <- df_muertes %>% na.omit() # limpio NA
 
 
+## Dia primer muerte por comuna ---------------
+# Limitado por fecha de primer reporte: 12-06-2020
+df_muerteZero <- df_muertes %>% 
+  filter(casos_fallecidos!=0) %>% #Filtro que haya casos
+  group_by(codigo_comuna,comuna) %>% 
+  summarise(dia_muerteZero=min(fecha, na.rm=T)) %>% ungroup()
+
+
 ## Muertes totales a la fecha -----------
 # Filtro fechas mas reciente (dado que son muertes acumuladas)
 fecha_muertes <- df_muertes$fecha %>% max()
@@ -26,7 +34,11 @@ df_muertes$poblacion %>% sum()
 ## Parametros --------
 df_muertes <- df_muertes %>% 
   mutate(tasa_mortalidad=casos_fallecidos/poblacion*1e5) %>% 
-  select(codigo_comuna, casos_fallecidos, tasa_mortalidad)
+  left_join(df_muerteZero %>% select(-comuna)) %>% 
+  mutate(dias_primerMuerte=(fecha-dia_muerteZero) %>% 
+           as.numeric(units="days")) %>% 
+  select(codigo_comuna, casos_fallecidos, tasa_mortalidad,
+         dias_primerMuerte)
 
-rm(url)
+rm(url, df_muerteZero)
 ## EoF
