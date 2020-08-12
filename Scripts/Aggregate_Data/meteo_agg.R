9abc46e315ec38876b252ba8de98cc670ca6b4f5
Pablo <pmbusch@uc.cl>
2020-08-12 18:05:03 -0400

    update data meteorologia

--- Scripts/Aggregate_Data/meteo_agg.R
+++ Scripts/Aggregate_Data/meteo_agg.R
@@ -7,21 +7,30 @@
 df <-read_rds("Data/Data_Modelo/Datos_Meteorologia_raw.rsd")
 source("Scripts/00-Funciones.R", encoding = "UTF-8")
 
+df$tipo %>% unique()
+df$date %>% year() %>% unique()
+
+# Numero estaciones: 398
+df$estacion %>% unique() %>% length()
+
 ## Agregar a nivel comunal y promediar por años ----------------
 # Promedio simple: Que va primero: estacion o años??
-df <- df %>% 
+df_meteo <- df %>% 
   mutate(year=year(date)) %>% 
-  filter(year %in% c(2016,2017,2018,2019) &
-           tipo %in% c("tmed", "hr","heating_degree")) %>%
-  # filter(season %in% c("winter","summer")) %>% # solo verano e invierno
+  filter(year %in% 2010:2019 &
+           tipo %in% c("tmed", "hr","heating_degree_18","heating_degree_15")) %>% 
   group_by(codigo_comuna, tipo, season) %>% 
   summarise(valor=mean(valor, na.rm=T)) %>% ungroup() %>% 
   mutate(tipo=paste(tipo, season, sep="_"),
-         season=NULL)
+         season=NULL) %>% 
+  filter(!is.nan(valor))
  
 # Expandir datos
-df <- df %>% spread(tipo,valor)
+df_meteo <- df_meteo %>% spread(tipo,valor)
 
-saveRDS(df, "Data/Data_Modelo/Datos_Meteorologia.rsd")
+# N comunsa con al menos 1 dato de meteorologia
+df_meteo %>% nrow()
+
+saveRDS(df_meteo, "Data/Data_Modelo/Datos_Meteorologia.rsd")
 
 ## EoF
