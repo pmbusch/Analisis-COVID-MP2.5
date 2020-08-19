56179ecb605f698defcd51c34aaf52738760afa2
Pablo <pmbusch@uc.cl>
2020-08-19 11:14:34 -0400

    Fix convergence model error

--- Scripts/05-AnalisisTransversal.R
+++ Scripts/05-AnalisisTransversal.R
@@ -5,23 +5,22 @@
 ## Fuente: Bates, D., Mächler, M., Bolker, B., & Walker, S. (2014). Fitting linear mixed-effects models using lme4. arXiv preprint arXiv:1406.5823.
 ## PBH Julio 2020
 
+## Librerias ------
 theme_set(theme_bw(16)+theme(panel.grid.major = element_blank()))
 file_name <- "Figuras/Analisis_transversal/%s.png"
 source("Scripts/00-Funciones.R", encoding = "UTF-8")
-
 
 library(MASS)
 library(lme4)
 library(glmmTMB)
 library(gamm4)
 
-# source("Scripts/01-LoadAllData", encoding = "UTF-8") 
 ## Carga Datos a nivel de comuna-----
-
+# source("Scripts/01-LoadAllData", encoding = "UTF-8") 
 df_modelo %>% na.omit() %>% nrow() # dimension todas las variables
 
 
-## MODELO INICIAL-----
+## MODELO DEFAULT-----
 # Varaibles se seleccionaron con ayuda de Step, pero con logica externa, 
 # segun cuales podrian ser explicativas (y usadas en otros estudios)
 df_modelo %>% names()
@@ -40,10 +39,9 @@ df <- df_modelo %>%
 df %>% na.omit() %>% dim() #dimension
 df <- df %>% na.omit()
 
-## Modelo  -----------
+
 ## Poblacion va a off_set, dado que se estima la tasa
 # Buena explicacion: https://stats.stackexchange.com/questions/11182/when-to-use-an-offset-in-a-poisson-regression
-
 mod <- glmer.nb(casos_fallecidos ~ 
                   mp25 +
                   scale(densidad_pob) + scale(`15-44`) + scale(`65+`) +
@@ -62,7 +60,7 @@ mod <- glmer.nb(casos_fallecidos ~
 # Summary
 summary(mod)
 
-## MRR para cada variable -------
+## Calculo MRR -------
 # Se interpreta como el aumento relativo en la tasa de mortalidad covid por 1 ug/m3
 # Dado que los confundentes estan estandarizados, su MRR se interepreta como 
 # variacion relativa al aumento en 1 desviacion estandar de la variable confundente
@@ -82,7 +80,6 @@ est <- est %>% mutate(codes=case_when(
   p_value<0.05 ~ "*",
   p_value<0.1 ~ ".",
   T ~ ""))
-
 
 ## Tabla coeficientes
 est %>% 
@@ -122,7 +119,7 @@ est_mrr %>%
   align(j=1, align = "left", part="all") %>% 
   footnote(j=5, value=as_paragraph(foot_note), part="header", inline=T)
   # print(preview="pptx")
-
+  # print(preview="docx")
 rm(foot_note)
 
 ## Figura MRR
@@ -163,18 +160,16 @@ plot(mod,type=c("p","smooth")) ## fitted vs residual
 plot(mod,sqrt(abs(resid(.)))~fitted(.), type=c("p","smooth")) ## scale-location
 lattice::qqmath(mod,id=0.05) ## quantile-quantile
 
-
 ## Prediction ------
 # type=responde https://stackoverflow.com/questions/47486589/what-is-the-difference-between-type-response-terms-and-link-in-predict-f
 # type=link entrega el logaritmo natural de la tasa de mortalidad (variable dependiente)
 # MODELO PREDICE CASOS FALLECIDOS!
-
-df %>% 
-  mutate(y=predict(mod,type="response"),
-         res=casos_fallecidos-y,
-         residuals=residuals(mod, type="response")) %>% 
-  dplyr::select(nombre_comuna,poblacion,casos_fallecidos,y,res,residuals,
-                mp25,densidad_pob, tasa_mortalidad) %>% view()
+# df %>% 
+#   mutate(y=predict(mod,type="response"),
+#          res=casos_fallecidos-y,
+#          residuals=residuals(mod, type="response")) %>% 
+#   dplyr::select(nombre_comuna,poblacion,casos_fallecidos,y,res,residuals,
+#                 mp25,densidad_pob, tasa_mortalidad) %>% view()
 
 # comparacion prediccion
 df %>% 
@@ -183,11 +178,7 @@ df %>%
   geom_line(aes(y = predict(mod,type="response")/poblacion*1e5),
             size=1, col="red", alpha=.5)+
   labs(x="Concentración MP2.5 2016-2019 [ug/m3]", 
-       y="Tasa Mortalidad COVID [muertes/100mil hab]")+
-  theme_bw(16)
-
-
-
+       y="Tasa Mortalidad COVID [muertes/100mil hab]")
 
 df %>% 
   mutate(rm=if_else(region=="M","RM","Resto Chile") %>% factor()) %>% 
@@ -202,11 +193,9 @@ df %>%
        y="Predicción")+
   ggtitle("Tasa Mortalidad COVID [muertes/100mil hab]")+
   theme(panel.grid.minor = element_blank())
-
-
 f_savePlot(last_plot(), sprintf(file_name,"Obs_vs_Pred"), dpi=100)
 
-## Mapa comparativo
+## Mapa comparativo ---------
 # detach("package:gamm4", unload = TRUE)
 source("Scripts/Analisis_Exploratorios/f_figuras.R", encoding = "UTF-8")
 df_map <- df %>%
@@ -258,9 +247,11 @@ library(caret)
 
 ## Creo df solo con variables numericas de interes (y fuera de COVID)
 df <-  df_modelo %>% select_if(is.numeric) %>% 
-  dplyr::select(-casos_fallecidos, -poblacion,
-                -tasa_contagios,-casos_confirmados,
-         -pcr_region, -perc_letalidad,-defunciones,-tasa_mortalidad_all) %>% 
+  dplyr::select(
+    # -casos_fallecidos, -poblacion,
+    -tasa_mortalidad,
+    -tasa_contagios,-casos_confirmados,
+    -pcr_region, -perc_letalidad,-defunciones,-tasa_mortalidad_all) %>% 
   na.omit()
 
 # Columnas a remover dado que serian redundantes por su correlacion con otras variables
@@ -273,35 +264,58 @@ df[,cols] %>% names()
 
 df <- df[,-cols]
 
-glm_fit <- train(tasa_mortalidad ~ ., 
+## Train with ML
+getModelInfo("glmStepAIC")
+modelLookup("glmStepAIC")
+
+# AIC estimates the relative amount of information lost by a given model: 
+# the less information a model loses, the higher the quality of that model
+glm_fit <- train(casos_fallecidos~ . +offset(log(poblacion)),
+  # tasa_mortalidad ~ ., 
                  data=df,
-                 # method="glm.nb",
                  method="glmStepAIC",
                  # method="glmnet",
-                 # metric="RMSE",
+                 family="poisson",
+                 link="log",
                  na.action = na.omit)
-
 glm_fit
 summary(glm_fit)
 
-## Otra prueba
-df <-  df_modelo %>% 
-  dplyr::select(-codigo_comuna, -nombre_comuna,-codigo_provincia,-nombre_provincia,
-                -codigo_region,-nombre_region,-region,
-                -tasa_mortalidad,-geometry,
-                -tasa_contagios,-casos_confirmados,
-                -pcr_region, -perc_letalidad,-defunciones,-tasa_mortalidad_all) %>% 
-  na.omit()
+# NOTAS: Algoritmo step funciona con familia Poisson, y sin efectos aleatorios
 
-# Debo cambiar la semilla, sino no encuentra solucion factible
-set.seed(30, sample.kind="Rounding")
-glm_fit <- train(casos_fallecidos ~ .+ offset(log(poblacion)), 
+
+# Poisson GLM peude dar el valor inicial del theta
+# Theta is usually interpreted as a measure of overdispersion with respect to the Poisson distribution
+# https://stats.stackexchange.com/questions/10419/what-is-theta-in-a-negative-binomial-regression-fitted-with-r
+getME(mod,"glmer.nb.theta") #valor del modelo origal
+# getME(glm_fit,"glmer.nb.theta")
+glm_fit_nb <- train(casos_fallecidos~ . +offset(log(poblacion)),
                  data=df,
-                 method="glm.nb",
+                 method="glmStepAIC",
+                 family=negative.binomial(theta=279.9946,link="log"),
                  na.action = na.omit)
+glm_fit_nb
+summary(glm_fit_nb)
 
-glm_fit
-summary(glm_fit)
+
+# ## Otra prueba
+# df <-  df_modelo %>% 
+#   dplyr::select(-codigo_comuna, -nombre_comuna,-codigo_provincia,-nombre_provincia,
+#                 -codigo_region,-nombre_region,-region,
+#                 -tasa_mortalidad,-geometry,
+#                 -tasa_contagios,-casos_confirmados,
+#                 -pcr_region, -perc_letalidad,-defunciones,-tasa_mortalidad_all) %>% 
+#   na.omit()
+# 
+# # Debo cambiar la semilla, sino no encuentra solucion factible
+# set.seed(3, sample.kind="Rounding")
+# glm_fit <- train(casos_fallecidos ~ .+ offset(log(poblacion)), 
+#                  data=df,
+#                  method="glm.nb",
+#                  na.action = na.omit)
+# 
+# glm_fit
+# summary(glm_fit)
 
 
 # cols <- df %>% 
@@ -324,34 +338,99 @@ summary(glm_fit)
 #   cat(i, "\n")
 #   set.seed(i, sample.kind="Rounding")
 #   glm_fit <- tryCatch({
-#     train(casos_fallecidos ~ .+ offset(log(poblacion)), 
+#     train(casos_fallecidos ~ .+ offset(log(poblacion)),
 #                    data=df,
 #                    method="glm.nb",
 #                    na.action = na.omit)
 #   }, error = function(cond) return(NULL))
 #   if (!is.null(glm_fit)) break
 # }
-# 
+
 # glm_fit
 # summary(glm_fit)
-
-
 
 # eliminate variables with a low t-statistic
 # rfe(df,df$tasa_mortalidad, rfeControl=rfeControl(functions=lmFuncs))
 
 
+### PRUEBAS OTROS MODELOS
+df <- df_modelo %>% 
+  dplyr::select(nombre_comuna,region, poblacion,tasa_mortalidad,
+                casos_fallecidos,mp25,
+                densidad_pob, `65+`, `15-44`, perc_puebloOrig, perc_rural,
+                dias_primerContagio, dias_cuarentena,tasa_camas,
+                perc_lenaCocina,
+                ingresoTotal_media,perc_menor_media,
+                perc_fonasa_A, perc_fonasa_D, perc_isapre,
+                tmed_summer, tmed_winter, 
+                heating_degree_15_summer, heating_degree_15_winter,
+                heating_degree_18_summer, heating_degree_18_winter)
 
 
+## Sin MP2.5---------
+mod_sinMP <- glmer.nb(casos_fallecidos ~ 
+                  scale(densidad_pob) + scale(`15-44`) + scale(`65+`) +
+                  scale(perc_puebloOrig) + scale(perc_rural) +
+                  scale(dias_primerContagio) +  scale(dias_cuarentena) + 
+                  scale(tasa_camas) + 
+                  scale(perc_lenaCocina) + 
+                  scale(log(ingresoTotal_media)) + scale(perc_menor_media) + 
+                  scale(perc_fonasa_A) + scale(perc_fonasa_D) +
+                  scale(tmed_summer) + scale(tmed_winter) + 
+                  scale(heating_degree_15_summer) + scale(heating_degree_15_winter) +
+                  (1|region)+
+                  offset(log(poblacion)), 
+                data = df,
+                na.action=na.omit)
+summary(mod_sinMP)
 
-## MODELO SOLO MP2.5---------
+# Pq no converge?
+# https://rstudio-pubs-static.s3.amazonaws.com/33653_57fc7b8e5d484c909b615d8633c01d51.html
+# Check singularity
+tt <- getME(mod_sinMP,"theta")
+ll <- getME(mod_sinMP,"lower")
+min(tt[ll==0])
+# Double-checking gradient calculations
+derivs1 <- mod_sinMP@optinfo$derivs
+sc_grad1 <- with(derivs1,solve(Hessian,gradient))
+max(abs(sc_grad1))
+# restart (LO SOUCIONA!)
+ss <- getME(mod_sinMP,c("theta","fixef"))
+m2 <- update(mod_sinMP,start=ss,control=glmerControl(optCtrl=list(maxfun=2e4)))
+summary(m2)
+# try different optimizer  (LO SOUCIONA!)
+m3 <- update(mod_sinMP,start=ss,control=glmerControl(optimizer="bobyqa",
+                                                 optCtrl=list(maxfun=2e5)))
+summary(m3)
+
+
+## Sin RM---------
+mod_sinRM <- glmer.nb(casos_fallecidos ~ 
+                        mp25 +
+                        scale(densidad_pob) + scale(`15-44`) + scale(`65+`) +
+                        scale(perc_puebloOrig) + scale(perc_rural) +
+                        scale(dias_primerContagio) +  scale(dias_cuarentena) + 
+                        scale(tasa_camas) + 
+                        scale(perc_lenaCocina) + 
+                        scale(log(ingresoTotal_media)) + scale(perc_menor_media) + 
+                        scale(perc_fonasa_A) + scale(perc_fonasa_D) +
+                        scale(tmed_summer) + scale(tmed_winter) + 
+                        scale(heating_degree_15_summer) + scale(heating_degree_15_winter) +
+                        (1|region)+
+                        offset(log(poblacion)), 
+                      data = df %>% filter(region!="M"),
+                      na.action=na.omit)
+summary(mod_sinRM)
+exp(summary(mod_sinRM)[10]$coefficients[2,1]) # exponencial coeficiente MP2.5
+
+
+## Solo MP2.5---------
 modMP <- glmer.nb(casos_fallecidos ~ mp25 +
                   (1|region)+
                     offset(log(poblacion)),
-                  data = df)
-
+                  data = df,
+                  na.action=na.omit)
 summary(modMP)
 exp(summary(modMP)[10]$coefficients[2,1]) # exponencial coeficiente MP2.5
 
-
-
+## EoF
