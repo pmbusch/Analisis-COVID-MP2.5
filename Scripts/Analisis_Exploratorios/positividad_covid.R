b3d31812c3fb4719b9a8fe4c58e1154c2f059bec
Pablo <pmbusch@uc.cl>
2020-09-14 19:49:04 -0300

    Fix posititive test: calculeta as moving average of 7 days

--- Scripts/Analisis_Exploratorios/positividad_covid.R
+++ Scripts/Analisis_Exploratorios/positividad_covid.R
@@ -17,39 +17,56 @@ df_casos_tiempo_region <- df_casos_tiempo %>%
   summarise(casos=sum(casos, na.rm = T)) %>% ungroup()
 
 ## Join PCR + casos --------
+dicc_region <- mapa_comuna %>% 
+  group_by(codigo_region, region) %>% 
+  summarise(count=n()) %>% ungroup() %>% 
+  mutate(geometry=NULL, count=NULL)
+
 df_pcr_tiempo <- df_pcr_tiempo %>% 
   left_join(df_casos_tiempo_region) %>% 
-  select(-region) %>% left_join(mapa_comuna %>% select(codigo_region, region))
+  select(-region) %>% left_join(dicc_region)
+rm(dicc_region)
 
-# Positividad
-df_pcr_tiempo <- df_pcr_tiempo %>% 
-  mutate(positividad=casos/numero*100) %>% 
-  filter(positividad<100)
+df_pcr_tiempo <- df_pcr_tiempo %>% na.omit()
+
+# Positividad: Moving average 7 days centered
+library(zoo)
+df <- df_pcr_tiempo %>% 
+  arrange(region, fecha) %>% 
+  mutate(casos_cmean=rollmean(casos, k=7, fill=NA),
+         numero_cmean=rollmean(numero, k=7, fill=NA),
+         positividad=casos_cmean/numero_cmean*100)
 
 
 ## Serie tiempo region -------------
 # Añado promedio nacional
 df_pcr_tiempo_nac <- df_pcr_tiempo %>%
   group_by(fecha) %>% 
-  summarise(positividad=sum(casos,na.rm=T)/
-              sum(numero,na.rm=T)*100) %>% ungroup() %>% 
-  filter(positividad<100) %>% 
+  summarise(casos=sum(casos,na.rm=T),
+            numero=sum(numero,na.rm=T)) %>% ungroup() %>% 
+  arrange(fecha) %>% 
+  mutate(casos_cmean=rollmean(casos, k=7, fill=NA),
+         numero_cmean=rollmean(numero, k=7, fill=NA),
+         positividad=casos_cmean/numero_cmean*100) %>% 
   mutate(region="Nacional")
 
 df_pcr_tiempo_reg <- df_pcr_tiempo %>%
   group_by(region, fecha) %>% 
-  summarise(positividad=sum(casos,na.rm=T)/
-              sum(numero,na.rm=T)*100) %>% ungroup() %>% 
-  filter(positividad<100) %>% 
+  summarise(casos=sum(casos,na.rm=T),
+            numero=sum(numero,na.rm=T)) %>% ungroup() %>% 
+  arrange(region, fecha) %>% 
+  mutate(casos_cmean=rollmean(casos, k=7, fill=NA),
+         numero_cmean=rollmean(numero, k=7, fill=NA),
+         positividad=casos_cmean/numero_cmean*100) %>% 
   rbind(df_pcr_tiempo_nac)
 
 df_pcr_tiempo_reg %>% 
   filter(!is.na(region)) %>% 
   ggplot(aes(x=fecha, y=positividad))+
   geom_line()+
-  # facet_grid(region~., scales = "free", space="free")
   facet_wrap(~region)+
-  labs(x="", y="% Positivdad")+
+  labs(x="", y="% Positividad")+
+  ggtitle(" Promedio Móvil (7 días) % Positividad PCR")
   theme(axis.text.x = element_text(angle = 90))
 f_savePlot(last_plot(), sprintf(file_name,"PositividadRegion"))
 rm(df_pcr_tiempo_reg, df_pcr_tiempo_nac)
