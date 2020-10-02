5f31191032cc58cabdb9f8fa09061912e2d359d2
Pablo <pmbusch@uc.cl>
2020-10-01 23:02:35 -0300

    Comparison between months

--- Scripts/Analisis_Exploratorios/exceso_muertes.R
+++ Scripts/Analisis_Exploratorios/exceso_muertes.R
@@ -10,12 +10,17 @@ source("Scripts/Aggregate_Data/poblacion_agg.R", encoding = "UTF-8")
 source("Scripts/00-Funciones.R", encoding = "UTF-8")
 
 # Deis
-df_deis <- read_delim("Data/Data_Original/DEIS/DEFUNCIONES_FUENTE_DEIS_2016_2020_17092020.csv",
-                      delim = ";",col_names = F,
-                      col_types = "dDcddccccccccccccccccccccc",
-                      locale = locale(encoding = "windows-1252"))
+# lectura
+fecha_deis <- "24-09-2020"
+df_deis <- read_delim(paste(
+  "Data/Data_Original/DEIS/DEFUNCIONES_FUENTE_DEIS_2016_2020_",
+  fecha_deis %>% str_remove_all("-"),".csv",sep=""),
+  delim = ";",col_names = T,
+  col_types = "Dcddccccccccccccccccccccc",
+  locale = locale(encoding = "windows-1252",
+                  date_format = "%d-%m-%Y"))
 spec(df_deis)
-names(df_deis) <- c("year","date","sexo","edad_tipo","edad",
+names(df_deis) <- c("date","sexo","edad_tipo","edad",
                     "codigo_comuna","comuna","region",
                     "diag1","cap_diag1","glosa_cap_diag1",
                     "grupo_diag1","glosa_grupo_diag1",
@@ -25,6 +30,9 @@ names(df_deis) <- c("year","date","sexo","edad_tipo","edad",
                     "grupo_diag2","glosa_grupo_diag2",
                     "categ_diag2","glosa_categ_diag2",
                     "subcateg_diag2","glosa_subcateg_diag2")
+
+## Add year: antes estaba y lo borarron, como cambian el formato semana a semana....
+df_deis <- df_deis %>% mutate(year=year(date))
 
 # Factores
 df_deis$year %>% unique()
@@ -72,6 +80,63 @@ df_deis_tiempo %>%
   guides(colour = guide_legend(reverse=T))+
   scale_y_continuous(labels = function(x) format(x, big.mark = " ",scientific = FALSE))
 f_savePlot(last_plot(), sprintf(file_name,"ExcesoMuertes_Mes"))
+
+## Comparacion mes sin acumular ---------
+# Solicitador por Guille
+df_deis_tiempo %>% 
+  filter(year %in% c(2019,2020)) %>% 
+  mutate(month=month(date)) %>% 
+  group_by(year,month) %>% 
+  summarise(muertes=sum(muertes, na.rm=T)) %>% ungroup() %>% 
+  mutate(date=paste("1",month,year,sep="-") %>% strptime("%d-%m-%Y") %>% as_date()) %>% 
+  ggplot(aes(month(date, label=TRUE, abbr=TRUE), muertes, fill=year))+
+  geom_col(stat="identity", position = "dodge")+
+  labs(x="", y="Muertes en Chile", col="Año")+
+  # scale_fill_viridis_d()+
+  guides(fill = guide_legend(reverse=T))+
+  coord_cartesian(expand = F)+
+  scale_y_continuous(labels = function(x) format(x, big.mark = " ",scientific = FALSE))
+f_savePlot(last_plot(), sprintf(file_name,"ExcesoMuertes_Mes_sinAcc"))
+
+## Comparacion mes delta sin acumular ---------
+# Solicitador por Guille
+df_deis_tiempo %>% 
+  mutate(month=month(date)) %>% 
+  filter(year %in% c(2019,2020) & month<9) %>% 
+  group_by(year,month) %>% 
+  summarise(muertes=sum(muertes, na.rm=T)) %>% ungroup() %>% 
+  spread(year, muertes) %>% 
+  mutate(dif=`2020`-`2019`) %>% 
+  mutate(date=paste("1",month,"2020",sep="-") %>% strptime("%d-%m-%Y") %>% as_date()) %>% 
+  ggplot(aes(month(date, label=TRUE, abbr=TRUE), dif))+
+  geom_col(fill="brown")+
+  geom_text(aes(label=dif), size=5, vjust=-0.8)+
+  labs(x="", y="Diferencia muertes en Chile 2020-2019", col="Año")+
+  # scale_fill_viridis_d()+
+  guides(fill = guide_legend(reverse=T))+
+  coord_cartesian(ylim=c(0,6500), expand=F)+
+  scale_y_continuous(labels = function(x) format(x, big.mark = " ",scientific = FALSE))
+f_savePlot(last_plot(), sprintf(file_name,"ExcesoMuertes_Mes_sinAcc_delta"))
+
+
+## Comparacion mes delta sin acumular porcentual ---------
+df_deis_tiempo %>% 
+  mutate(month=month(date)) %>% 
+  filter(year %in% c(2019,2020) & month<9) %>% 
+  group_by(year,month) %>% 
+  summarise(muertes=sum(muertes, na.rm=T)) %>% ungroup() %>% 
+  spread(year, muertes) %>% 
+  mutate(dif=((`2020`-`2019`)/`2019`*100) %>% round(2)) %>% 
+  mutate(date=paste("1",month,"2020",sep="-") %>% strptime("%d-%m-%Y") %>% as_date()) %>% 
+  ggplot(aes(month(date, label=TRUE, abbr=TRUE), dif))+
+  geom_col(fill="brown")+
+  geom_text(aes(label=dif), size=5, vjust=-0.8)+
+  labs(x="", y="% Aumento porcentual muertes \n por mes Chile 2020-2019", col="Año")+
+  # scale_fill_viridis_d()+
+  guides(fill = guide_legend(reverse=T))+
+  coord_cartesian(ylim=c(0,60), expand=F)+
+  scale_y_continuous(labels = function(x) format(x, big.mark = " ",scientific = FALSE))
+f_savePlot(last_plot(), sprintf(file_name,"ExcesoMuertes_Mes_sinAcc_delta_perc"))
 
 
 ## EoF
